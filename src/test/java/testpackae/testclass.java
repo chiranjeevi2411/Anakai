352d45c2af278adb5e88c6c1160e7722b814abe4
chiranjeevi2411 <pgchiranjeevi@gmail.com>
2023-08-17 07:27:26 +0530

    added new code

--- src/test/java/testpackae/testclass.java
+++ src/test/java/testpackae/testclass.java
@@ -4,7 +4,7 @@ public class testclass {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("welcome to singapore & malaysia");
+		System.out.println("welcome to singapore & malaysia @ india ");
 	}
 
 }
