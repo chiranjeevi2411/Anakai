1e820bd58921ef101b5ae0e3bb7b7194b1dc000b
chiranjeevi2411 <pgchiranjeevi@gmail.com>
2023-08-17 07:39:26 +0530

    own project

--- src/test/java/testpackae/testclass.java
+++ src/test/java/testpackae/testclass.java
@@ -4,7 +4,7 @@ public class testclass {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("welcome to singapore & malaysia @ india ");
+		System.out.println("welcome to singapore & malaysia @ india & japan america ");
 	}
 
 }
